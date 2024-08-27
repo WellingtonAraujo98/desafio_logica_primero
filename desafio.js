6cb870467699990e6a036287aaa29f245c95ccc2
WellingtonAraujo98 <wellington.a14@hotmail.com>
2024-08-27 12:21:07 -0300

    Termino do projeto e correções

--- desafio.js
+++ desafio.js
@@ -5,15 +5,31 @@ let xp = parseInt(prompt("Digite o xp atual: "));
 let nivel;
 
 switch (true) {
-    case (xp < 1000):
+    case (xp < 250):
         nivel = "Ferro";
         break;
-    case (xp <= 2000):
+    case (xp <= 700):
         nivel = "Bronze";
         break;
-    case (xp <= 3000):
-        nivel = "safdsa";
+    case (xp <= 2500):
+        nivel = "Prata";
         break;
-}
+    case (xp <= 10000):
+        nivel = "Ouro";
+        break;
+    case (xp <= 25000):
+        nivel = "Platina";
+        break;
+    case (xp <= 50000):
+        nivel = "Imortal";
+        break;
+    case (xp <= 100000):
+        nivel = "Radiante";
+        break;
+    case (xp > 100000):
+        nivel = "DEUS SUPREMO";
+        break;
+    
+}   
 
 alert("O Heroi de nome **${nome}** esta no nivel **${nivel}**");
